f5986716a0846c0efbeaa7f9834e8a4c28861440
Timur <timur.pnz@mail.ru>
2024-02-28 20:41:05 +0300

    Добавил ДЗ№4 по Java Core

--- src/main/hw_4/Program.java
+++ src/main/hw_4/Program.java
@@ -4,9 +4,11 @@ public class Program {
     public static void main(String[] args) {
         try {
             Account account = new Account(1000);
+            System.out.printf("Текущий баланс: %.1f\n", account.getBalance());
             account.deposit(500);
             account.withdraw(300);
             account.withdraw(1200);
+            System.out.printf("Текущий баланс: %.1f\n", account.getBalance());
         } catch (NegativeBalanceException | InsufficientFundsException e) {
             System.out.println("Ошибка: " + e.getMessage());
         } catch (IllegalArgumentException e) {
